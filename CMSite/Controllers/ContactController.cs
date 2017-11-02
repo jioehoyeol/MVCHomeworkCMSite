0efe1d3d4377bb438e1efb77c655247156bbdb76
jioehoyeol <33124438+jioehoyeol@users.noreply.github.com>
2017-11-02 21:33:23 +0800

    第一堂課作業完成

---
+++ CMSite/Controllers/ContactController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CMSite.Models;
+
+namespace CMSite.Controllers
+{
+    public class ContactController : Controller
+    {
+        private CustomerEntities db = new CustomerEntities();
+
+        // GET: Contact
+        public ActionResult Index(string keyword)
+        {
+            IQueryable<Models.客戶聯絡人> searchResult = null;
+
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                searchResult = db.客戶聯絡人.Where(o => o.姓名.Contains(keyword));
+            }
+            else
+            {
+                searchResult = db.客戶聯絡人.Include(客 => 客.客戶資料);
+            }
+
+            return View(searchResult.ToList());
+        }
+
+        // GET: Contact/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+            if (客戶聯絡人 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶聯絡人);
+        }
+
+        // GET: Contact/Create
+        public ActionResult Create()
+        {
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱");
+            return View();
+        }
+
+        // POST: Contact/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,客戶Id,職稱,姓名,Email,手機,電話")] 客戶聯絡人 客戶聯絡人)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!CheckEmailDuplicate(客戶聯絡人.Id, 客戶聯絡人.客戶Id, 客戶聯絡人.Email))
+                {
+                    db.客戶聯絡人.Add(客戶聯絡人);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("Email", "輸入的 Email 已存在，請重新輸入！");
+                }
+            }
+
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶聯絡人.客戶Id);
+            return View(客戶聯絡人);
+        }
+
+        // GET: Contact/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+            if (客戶聯絡人 == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶聯絡人.客戶Id);
+            return View(客戶聯絡人);
+        }
+
+        // POST: Contact/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,客戶Id,職稱,姓名,Email,手機,電話")] 客戶聯絡人 客戶聯絡人)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!CheckEmailDuplicate(客戶聯絡人.Id, 客戶聯絡人.客戶Id, 客戶聯絡人.Email))
+                {
+                    db.Entry(客戶聯絡人).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("Email", "輸入的 Email 已存在，請重新輸入！");
+                }
+            }
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶聯絡人.客戶Id);
+            return View(客戶聯絡人);
+        }
+
+        // GET: Contact/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+            if (客戶聯絡人 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶聯絡人);
+        }
+
+        // POST: Contact/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+            db.客戶聯絡人.Remove(客戶聯絡人);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        private bool CheckEmailDuplicate(int Id, int 客戶Id, string Email)
+        {
+            if (Id > 0)
+            {
+                return db.客戶聯絡人.Any(o => o.Id != Id && o.客戶Id == 客戶Id && o.Email == Email);
+            }
+            else
+            {
+                return db.客戶聯絡人.Any(o => o.客戶Id == 客戶Id && o.Email == Email);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CMSite/Controllers/DataController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CMSite.Models;
+
+namespace CMSite.Controllers
+{
+    public class DataController : Controller
+    {
+        private CustomerEntities db = new CustomerEntities();
+
+        // GET: Data
+        public ActionResult Index(string SearchId, string keyword, string sort)
+        {
+            IQueryable<Models.客戶資料> result = null;
+
+            //選資料
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                ViewBag.Keyword = keyword;
+                //result = db.客戶資料.Where(c => c.客戶名稱.Contains(keyword));
+
+                if (SearchId == "0")
+                    result = db.客戶資料.Where(e => e.客戶名稱.Contains(keyword));
+                else if (SearchId == "1")
+                    result = db.客戶資料.Where(e => e.統一編號.Contains(keyword));
+                else if (SearchId == "2")
+                    result = db.客戶資料.Where(e => e.電話.Contains(keyword));
+                else if (SearchId == "3")
+                    result = db.客戶資料.Where(e => e.傳真.Contains(keyword));
+                else if (SearchId == "4")
+                    result = db.客戶資料.Where(e => e.地址.Contains(keyword));
+                else if (SearchId == "5")
+                    result = db.客戶資料.Where(e => e.Email.Contains(keyword));
+            }
+            else
+            {
+                result = db.客戶資料.AsQueryable();
+            }           
+
+            //排序資料
+            if (!string.IsNullOrEmpty(sort))
+            {
+                ViewBag.NameOrder = string.Empty;
+                result = result.OrderByDescending(o => o.客戶名稱);
+            }
+            else
+            {
+                ViewBag.NameOrder = "name_desc";
+                result = result.OrderBy(o => o.客戶名稱);
+            }
+
+            return View(result.ToList());
+        }
+
+        // GET: Data/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // GET: Data/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Data/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料)
+        {
+            if (ModelState.IsValid)
+            {
+                db.客戶資料.Add(客戶資料);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(客戶資料);
+        }
+
+        // GET: Data/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // POST: Data/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(客戶資料).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(客戶資料);
+        }
+
+        // GET: Data/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // POST: Data/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            db.客戶資料.Remove(客戶資料);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
